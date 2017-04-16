87ec184ffd04c489e7345bc85c1273f9224a87d8
Alexandre Honoré <Alexandre@MacBook-Pro-de-Alexandre.local>
2017-04-16 11:50:12 +0200

    HTML

--- Projet_Web-Mobile/back/config/passport.js
+++ Projet_Web-Mobile/back/config/passport.js
@@ -3,18 +3,31 @@
  */
 var passportJWT = require('passport-jwt').Strategy;
 var User = require('../../../../models/User');
-var config = require('../../../../config/index');
+var Team = require('../../../../models/Team');
+var ExtractJwt = passportJWT.ExtractJwt;
+var config = require('../config/index')
 
 module.exports = function(passport) {
     var opts = {};
+    opts.jwtFromRequest = ExtractJwt.fromAuthHeader();
     opts.secretOrKey = "superSecretMotherFucker";
     passport.use(new passportJWT(opts, function(jwt_payload, done) {
-        User.findOne({id: jwt_payload.id}, function(err, user) {
+        User.findOne({username: jwt_payload.username}, function(err, user) {
             if (err) {
                 return done(err, false);
             }
             if (user) {
                 done(null, user);
+            } else {
+                done(null, false);
+            }
+        });
+        Team.findOne({id: jwt_payload.id}, function(err, team) {
+            if (err) {
+                return done(err, false);
+            }
+            if (team) {
+                done(null, team);
             } else {
                 done(null, false);
             }
--- Projet_Web-Mobile/back/web/router/api/index.js
+++ Projet_Web-Mobile/back/web/router/api/index.js
@@ -2,9 +2,9 @@ var router = require('express').Router();
 var passport = require('passport');
 var passportJWT = require("passport-jwt");
 //liste des routes de l'api
-router.use('/user', require('./user')); //passport.authenticate('jwt', { session: false}, Vérif du token
-router.use('/team', require('./team')); //passport.authenticate('jwt', { session: false}, Vérif du token
+router.use('/user',passport.authenticate('jwt', { session: false}), require('./user')); // Vérif du token
+router.use('/team',passport.authenticate('jwt', { session: false}), require('./team')); // Vérif du token
 router.use('/auth', require('./auth'));
-router.use('/api_ext', require('./api_ext'));
+router.use('/api_ext',passport.authenticate('jwt', { session: false}), require('./api_ext'));
 
 module.exports = router;
